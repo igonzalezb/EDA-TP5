de0137d4de4c4c6612bf89b07df7c88e8b658d2c
mmaspero <mmaspero@itba.edu.ar>
2017-04-10 14:36:16 -0300

    Add files via upload

---
+++ EDA-TP5/ServerStates.cpp
@@ -0,0 +1,197 @@
+#include "ServerStates.hpp"
+#include "events.hpp"
+
+extern "C" {
+#include "curses.h"
+}
+
+
+
+GenericState* Writing :: onData (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Writing");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: store data, send ACK");
+    
+    return NULL;
+}
+
+GenericState* Writing :: onLastData (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Writing");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: store data, send LAST_ACK, close file");
+    
+    GenericState* newState = new Idle();
+    
+    return newState;
+}
+
+GenericState* Writing :: onTimeout(GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Writing");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: resend ACK");
+    
+    return NULL;
+}
+
+GenericState* Writing :: onError (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Writing");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: close file");
+    
+    GenericState* newState = new Idle();
+    
+    return newState;
+}
+
+
+GenericState* Writing :: onExit (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Writing");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: close file");
+    
+    return NULL;
+}
+
+
+
+
+GenericState* Reading :: onAck(GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Reading");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: send data");
+    
+    return NULL;
+}
+
+
+GenericState* Reading :: onLastAck (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Reading");
+    
+   move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: close file");
+  
+    GenericState* newState = new Idle();
+    
+    return newState;
+}
+
+
+GenericState* Reading :: onTimeout(GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Reading");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: resend data");
+
+    
+    return NULL;
+}
+
+
+
+GenericState* Reading :: onError (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Reading");
+    
+   move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: close file");
+    
+    GenericState* newState = new Idle();
+
+    return newState;
+}
+
+
+GenericState* Reading :: onExit (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Reading");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: close file");
+    
+    return NULL;
+}
+
+GenericState* Idle:: onRrq (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Idle");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: open file");
+    
+    GenericState* newState = new Reading();
+    return newState;
+}
+
+GenericState* onWrq (GenericEvent * event)
+{
+    SimulationEvent * ev = (SimulationEvent *)event;
+    Point p = ev->getWhereToWrite();
+    
+    move((unsigned int)p.getX(), (unsigned int)p.getY());
+    printw("Estado = Idle");
+    
+    move((unsigned int)p.getX()+3, (unsigned int)p.getY());
+    printw("Accion ejecutada: open file");
+    
+    GenericState* newState = new Writing();
+    return newState;
+}
+
---
+++ EDA-TP5/ServerStates.hpp
@@ -0,0 +1,65 @@
+
+#ifndef ServerStates_hpp
+#define ServerStates_hpp
+
+#include <stdio.h>
+
+
+#include "GenericState.hpp"
+#include "GenericEvent.hpp"
+#include <string>
+
+
+using namespace std;
+
+class SimulationState : public GenericState
+{
+public:
+    SimulationState(string name) {this->name = name;}
+    
+    
+protected:
+    string name;
+};
+
+
+class Idle : public SimulationState
+{
+    
+public:
+    Idle () : SimulationState ("Idle"){};
+    
+    virtual GenericState* onRrq (GenericEvent * ev);
+    virtual GenericState* onWrq (GenericEvent * ev);
+  
+};
+ 
+
+class Writing : public SimulationState
+
+{
+public:
+    Writing() : SimulationState ("Writing"){};
+    
+    virtual GenericState* onData (GenericEvent * ev);
+    virtual GenericState* onLastData (GenericEvent * ev);
+    virtual GenericState* onTimeout(GenericEvent * ev);
+    virtual GenericState* onError (GenericEvent * ev);
+    virtual GenericState* onExit (GenericEvent * ev);
+};
+
+
+class Reading : public SimulationState
+{
+public:
+    Reading() : SimulationState ("Reading"){};
+    
+    
+    virtual GenericState* onAck(GenericEvent * ev);
+    virtual GenericState* onLastAck (GenericEvent * ev);
+    virtual GenericState* onTimeout(GenericEvent * ev);
+    virtual GenericState* onError (GenericEvent * ev);
+    virtual GenericState* onExit (GenericEvent * ev);
+};
+
+#endif /* ServerStates_hpp */
